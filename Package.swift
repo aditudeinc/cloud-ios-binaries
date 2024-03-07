89faad887485c31196044976233e68297508ec45
aditudedevs <mobile.team@aditude.io>
2024-03-07 19:24:10 +0200

    1.0.12

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "10.11.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "11.1.0")
     ],
     targets: [
         .target(
