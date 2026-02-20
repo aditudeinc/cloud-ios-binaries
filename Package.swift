7abfa836bab96a8a4387a11d8c1afce513cc2e0f
Abdulmedzid Pehlic <abdulmedzid.pehlic@klika.ba>
2026-02-20 09:36:14 +0100

    3.6.0

--- Package.swift
+++ Package.swift
@@ -13,16 +13,16 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/prebid/prebid-mobile-ios.git", exact: "3.2.0"),
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "12.14.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.0.0")
     ],
     targets: [
         .target(
             name: "_Tude",
             dependencies: [
                 "Tude",
+                "OMSDK_Prebidorg",
+                "PrebidMobile",
                 "DTBiOSSDK",
-                .product(name: "PrebidMobile", package: "prebid-mobile-ios"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
             ],
             path: "Tude",
@@ -33,6 +33,14 @@ let package = Package(
         .binaryTarget(
             name: "Tude",
             path: "Tude.xcframework"
+        ),
+        .binaryTarget(
+            name: "OMSDK_Prebidorg",
+            path: "OMSDK_Prebidorg.xcframework"
+        ),
+        .binaryTarget(
+            name: "PrebidMobile",
+            path: "PrebidMobile.xcframework"
         ),
         .binaryTarget(
             name: "DTBiOSSDK",
