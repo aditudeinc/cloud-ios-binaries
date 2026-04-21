093c789472cab463270345f520a9dfcf25c20481
Abdulmedzid Pehlic <abdulmedzid.pehlic@klika.ba>
2026-04-21 16:50:00 +0200

    3.7.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.0.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0")
     ],
     targets: [
         .target(
