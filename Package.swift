f1941e1e23a79d6b85777a5bd5c789e93c410b7f
Abdulmedzid Pehlic <abdulmedzid.pehlic@klika.ba>
2026-05-12 16:05:14 +0200

    Temp

--- Package.swift
+++ Package.swift
@@ -13,21 +13,21 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0"),
+        .package(url: "https://github.com/prebid/prebid-mobile-ios.git", from: "3.1.1"),
     ],
     targets: [
         .target(
             name: "_Tude",
             dependencies: [
                 "Tude",
-                "OMSDK_Prebidorg",
-                "PrebidMobile",
+                .product(name: "PrebidMobile", package: "prebid-mobile-ios"),
                 "DTBiOSSDK",
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
             ],
             path: "Tude",
             resources: [
-                .copy("PrivacyInfo.xcprivacy")
+                .copy("PrivacyInfo.xcprivacy"),
             ]
         ),
         .binaryTarget(
@@ -35,16 +35,8 @@ let package = Package(
             path: "Tude.xcframework"
         ),
         .binaryTarget(
-            name: "OMSDK_Prebidorg",
-            path: "OMSDK_Prebidorg.xcframework"
-        ),
-        .binaryTarget(
-            name: "PrebidMobile",
-            path: "PrebidMobile.xcframework"
-        ),
-        .binaryTarget(
             name: "DTBiOSSDK",
             path: "DTBiOSSDK.xcframework"
-        )
+        ),
     ]
 )
