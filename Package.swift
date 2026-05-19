ff55cdaa840d61f8debf3d6e724b5a979eb324db
Abdulmedzid Pehlic <abdulmedzid.pehlic@klika.ba>
2026-05-19 11:57:26 +0200

    3.8.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version:6.0
 import PackageDescription
- 
+
 let package = Package(
     name: "Tude",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v13),
     ],
     products: [
         .library(
@@ -13,38 +13,32 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: "13.2.0")
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "13.4.0"),
+        .package(url: "https://github.com/prebid/prebid-mobile-ios.git", from: "3.3.1"),
     ],
     targets: [
         .target(
             name: "_Tude",
             dependencies: [
                 "Tude",
-                "OMSDK_Prebidorg",
-                "PrebidMobile",
+                .product(name: "PrebidMobile", package: "prebid-mobile-ios"),
                 "DTBiOSSDK",
-                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
+                .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads"),
             ],
             path: "Tude",
             resources: [
-                .copy("PrivacyInfo.xcprivacy")
+                .copy("PrivacyInfo.xcprivacy"),
             ]
         ),
         .binaryTarget(
             name: "Tude",
-            path: "Tude.xcframework"
-        ),
-        .binaryTarget(
-            name: "OMSDK_Prebidorg",
-            path: "OMSDK_Prebidorg.xcframework"
-        ),
-        .binaryTarget(
-            name: "PrebidMobile",
-            path: "PrebidMobile.xcframework"
+            url: "https://github.com/aditudeinc/cloud-ios-binaries/releases/download/3.8.0/Tude.xcframework.zip",
+            checksum: "56ea6c134e2e32b445f73c65828dd34b551222b2ff7bfb5995d75f55b2d04dbb"
         ),
         .binaryTarget(
             name: "DTBiOSSDK",
-            path: "DTBiOSSDK.xcframework"
-        )
+            url: "https://github.com/aditudeinc/cloud-ios-binaries/releases/download/3.8.0/DTBiOSSDK.xcframework.zip",
+            checksum: "7699c6d340cf184cd9f0ed180bff3fd97e5bf6588160e1d33386f81f8a90db51"
+        ),
     ]
 )
